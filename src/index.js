2230d0d16a0f2a52f37221b228162143d3935a77
nectakin <kvs171005@gmail.com>
2024-08-10 18:04:06 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 
+// import { initMongoDB } from './db/initMongoDB.js';
+// import { startServer } from './server.js';
+
+// const bootstrap = async () => {
+//   await initMongoDB();
+//   startServer();
+// };
+
+// bootstrap();
 import initMongoConnection from './db/initMongoConnection.js';
 import setupServer from './server.js';
 
--- src/services/schemas/ContactSchema.js
+++ src/services/schemas/ContactSchema.js
@@ -1,30 +1,33 @@
-
-import mongoose from 'mongoose';
-
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
+import { model, Schema } from 'mongoose';
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, 'Phone number is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+    },
+    isFavourite: {
+      type: Boolean,
+      required: [true, 'IsFavourite is required'],
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: [true, 'ContactType is required'],
+      default: 'personal',
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: [true, 'Phone number is required'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-  },
-  isFavourite: {
-    type: Boolean,
-    required: [true, 'IsFavourite is required'],
-  },
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    required: [true, 'ContactType is required'],
-    default: 'personal',
-  },
-});
-
-const Contact = mongoose.model('contact', schema);
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+const Contact = model('contact', schema);
 export default Contact;
