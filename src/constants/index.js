d99f9846923ff55a508a196017f575189b905ad7
nectakin <kvs171005@gmail.com>
2024-08-21 16:28:46 +0300

    start

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,6 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
+export const ACCESS_TOKEN_EXPIRY = 15 * 60 * 1000;
+
+export const REFRESH_TOKEN_EXPIRY = 30 * 24 * 60 * 60 * 1000;
--- src/server.js
+++ src/server.js
@@ -2,16 +2,19 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import contactsRouter from '../src/routers/contacts.js';
+// import contactsRouter from '../src/routers/contacts.js';
 import env  from './utils/env.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT'));
 export const setupServer = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
@@ -19,7 +22,7 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(contactsRouter);
+  app.use(router);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
