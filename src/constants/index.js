34d8f05a90f1e8f5eff3494120cbdb542581147e
nectakin <kvs171005@gmail.com>
2024-09-01 14:00:21 +0300

    start

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,3 +27,4 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
   ENABLE_CLOUDINARY: 'ENABLE_CLOUDINARY',
 };
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -9,11 +9,13 @@ import { errorHandler } from './middleware/errorHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middleware/swaggerDocs.js';
 
 const PORT = Number(env('PORT'));
 export const setupServer = () => {
   const app = express();
-   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
